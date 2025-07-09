00e0ec062f0586822b5f275a0d9883aec9fe721b
unknown <yunusbraq@gmail.com>
2025-07-08 22:50:14 -0700

    new commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+
+  const [name, setName]=useState("");
+
+  const [email, setEmail]=useState("");
+
+  const [password, setPassword] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container bg-primary m-5 p-3">
+        <div className="row justify-content-center">
+          <div className="col-lg-6">
+            <form>
+
+              <div className="">
+                <label className="">Name</label>
+                <input type="text"
+                 className="form-control"
+                 value={name}
+                  onChange ={(e) => {e.target.value}}/>
+              </div>
+
+              <div>
+                <label className="">Email</label>
+                <input type="text" className="form-control"/>
+              </div>
+              <div>
+                <label className="">Address</label>
+                <input type="text" className="form-control"/>
+              </div>
+
+              <button className="btn btn-success form-control mt-3">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
